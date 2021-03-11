8f8287573722160b70649ab3edc988e8430b9cda
JesFlores <77745981+JesFlores@users.noreply.github.com>
2021-03-11 14:06:24 +0100

    modification prix import
    modification prix import

--- Tanoa-Life.Tanoa/configs/GameConfigs/Config_Vehicles.hpp
+++ Tanoa-Life.Tanoa/configs/GameConfigs/Config_Vehicles.hpp
@@ -6,7 +6,7 @@ class ADENIS_VEHICLES_INFO
 	steal_percentage=0.4;
 	sell_percentage=0.4;
 	transfert_percentage=0.15;
-	impound_percentage=0.2;
+	impound_percentage=0.80;
 	// based on vehicle's buyPrice
 };
 
