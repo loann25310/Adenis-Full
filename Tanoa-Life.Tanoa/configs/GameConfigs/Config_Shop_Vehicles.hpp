8fc8761b7e95f22b64da77b6280f95aa69ae73e8
CgSYannick <github@micronity.com>
2021-04-05 20:10:15 +0200

    Revert "Ajout VL motorsport"
    This reverts commit ac2a446960f097d128d9812cb73bcf6974948eb4.

--- Tanoa-Life.Tanoa/configs/GameConfigs/Config_Shop_Vehicles.hpp
+++ Tanoa-Life.Tanoa/configs/GameConfigs/Config_Shop_Vehicles.hpp
@@ -76,16 +76,7 @@ class ADENIS_SHOPS_VEHICLES
 			"AlessioGTR",
 			"V12_M600",
 			"AlessioCayman",
-			"Alessio918",
-			"AlessioAgera",
-			"V12_GHOST_BLANC",
-			"V12_ROYAL_BLANC",
-			"V12_DIVORB",
-			"V12_GT2RS_ROUGE",
-			"AlessioHuayra",
-			"V12_EMPEROR",
-			"AlessioPanamera",
-			"AlessioGallardoMat"
+			"Alessio918"
 		};
 	};
 	class CIV_concess_2: CIV_air
