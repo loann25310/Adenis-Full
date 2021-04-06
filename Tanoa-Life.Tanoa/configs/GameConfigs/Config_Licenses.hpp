3261f8784a6e38e4225011044c2cb19f795e25f2
JesFlores <77745981+JesFlores@users.noreply.github.com>
2021-04-06 17:33:48 +0200

    DEBUG NUKA
    DEBUG NUKA

--- Tanoa-Life.Tanoa/configs/GameConfigs/Config_Licenses.hpp
+++ Tanoa-Life.Tanoa/configs/GameConfigs/Config_Licenses.hpp
@@ -489,6 +489,19 @@ class ADENIS_LICENSES
 			};
 		};
 	};
+	class cook_nuka
+	{
+		name="Restaurateur";
+		class factions
+		{
+			class CIV
+			{
+				price=10150;
+				condition="!(isNull g_company) && {(((g_company getVariable ['company_info',['','','']]) select 2) isEqualTo 'cook')}";
+				search_shown[]={"WEST","EAST","GUER","CIV"};
+			};
+		};
+	};
 	class company_music_label
 	{
 		name="Label de musique";
